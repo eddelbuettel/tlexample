0ed78bb3fd687586bee29fc5a3c2b15b75311497
Dirk Eddelbuettel <edd@debian.org>
2026-06-03 09:04:17 -0500

    Robustify build against insufficient (macOS, mostly) setups

--- src/bar.cpp
+++ src/bar.cpp
@@ -1,4 +1,18 @@
 #include <Rcpp/Lighter>
+
+// We need to guard against the systems which we know cannot cope
+// This is approximative, and right now mostly for use at r-universe
+// where all Linux and Windows builds work (or r-release or r-devel)
+// but where macOS croaks on any x86_64 and on arm64-oldrel surviving
+// only for arm64-release
+#if (defined(__APPLE__) && defined(__MACH__) && (defined(__aarch64__) && R_VERSION >= R_Version(4,6,0))) || __cplusplus < 202002L
+
+void bar(std::string) {
+  Rcpp::Rcout << "Sorry, insufficient C++20 support on this platform.\n";
+}
+
+#else
+
 #include <tl.h>
 
 //' @title Simple example function
@@ -25,3 +39,5 @@ void bar(std::string s = "baz") {
   tl::set_level("debug");
   tl::debug("seen as level from C++ is now '{}'", tl::get_level());
 }
+
+#endif
